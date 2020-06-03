0555e994e8e89fa87405310e80f6926f4865e31d
Cedric Leroy <cedie73@hotmail.fr>
2020-06-03 16:41:52 -0500

    Fish config

---
+++ config.fish
@@ -0,0 +1,11 @@
+# *** fish config file ***
+# https://fishshell.com/
+# Location : ~/.config/fish/config.fish
+# default MacOS
+# Add /usr/local/bin/fish to /etc/shells
+# chsh -s /usr/local/bin/fish
+
+# Vim key binding
+fish_vi_key_bindings
+# https://starship.rs/
+starship init fish | source
