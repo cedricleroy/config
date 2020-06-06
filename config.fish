293a0bac83a6ed3238e791653ba8107103dc832a
Cedric Leroy <cedie73@hotmail.fr>
2020-06-05 22:54:29 -0500

    Tweak config.fish

--- config.fish
+++ config.fish
@@ -14,3 +14,18 @@ starship init fish | source
 # direnv:
 eval (direnv hook fish)
 
+alias vi=nvim
+alias vim=nvim
+alias poetry=~/.poetry/bin/poetry
+
+function venv
+    source venv/bin/activate.fish
+end
+
+function __check_rvm --on-variable PWD --description 'Activate venv if any'
+    if ls | grep venv > /dev/null
+        echo activate venv
+        source venv/bin/activate.fish
+    end
+end
+
